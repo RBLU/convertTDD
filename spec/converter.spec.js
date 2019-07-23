f52ca1060ee8ce653ba4e7692251978f5780551d
RBLU <reto.blunschi@youpers.com>
2019-07-23 09:05:55 +0200

    small update

--- spec/converter.spec.js
+++ spec/converter.spec.js
@@ -3,6 +3,6 @@ let convert = require('../src/converter');
 describe('converter lengths', function () {
 
     it('should convert m into m', function() {
-        expect(convert(5, 'm')).toEqual(5);
+        expect(convert(5, 'm', 'm')).toEqual(5);
     })
 });
